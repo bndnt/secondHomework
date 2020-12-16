b282eec05fb3fca3089a2c3c4fb940e586956356
bndnt <l.makushchenko@gmail.com>
2020-12-16 17:24:13 +0200

    second task

--- js.js
+++ js.js
@@ -1,25 +1,32 @@
 let a =2+2*2;
 switch (a) {
     case 1 :
-        alert('До решения еще далеко'); break;
+        console.log('До решения еще далеко'); break;
     case 2:
-        alert("До решения еще далеко"); break;
+        console.log("До решения еще далеко"); break;
     case 3:
-        alert("До решения еще далеко"); break;
+        console.log("До решения еще далеко"); break;
     case 4:
-        alert("До решения еще далеко"); break;
+        console.log("До решения еще далеко"); break;
     case 5:
-        alert("До решения еще далеко"); break;
+        console.log("До решения еще далеко"); break;
     case 6:
-        alert('Решение найдено'); break;
+        console.log('Решение найдено'); break;
     case 7:
-        alert('Решение уже найдено, но мы чего-то еще считаем'); break;
+        console.log('Решение уже найдено, но мы чего-то еще считаем'); break;
     case 8:
-        alert('Решение уже найдено, но мы чего-то еще считаем'); break;
+        console.log('Решение уже найдено, но мы чего-то еще считаем'); break;
     case 9:
-        alert('Решение уже найдено, но мы чего-то еще считаем'); break;
+        console.log('Решение уже найдено, но мы чего-то еще считаем'); break;
     case 10:
-        alert('Решение уже найдено, но мы чего-то еще считаем'); break;
+        console.log('Решение уже найдено, но мы чего-то еще считаем'); break;
     default:
-        alert('Решение не найдено');
+        console.log('Решение не найдено');
 }
+//
+let x =5;
+console.log(x);
+console.log(typeof x);
+x=String(x);
+console.log(x);
+console.log(typeof x);
